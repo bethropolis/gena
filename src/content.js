8d2e92477e37b814350a632539dad4aca82f0d8c
Bethuel <bethropolis@gmail.com>
2023-07-31 12:45:04 +0300

    first

---
+++ src/content.js
@@ -0,0 +1,30 @@
+// content.ts
+export const sidebarStructure = [
+  {
+    label: "Getting Started",
+    children: [
+      {
+        label: "demo",
+        path: "/docs/demo",
+      },
+      {
+        label: "Option 2",
+        path: "/advanced/option-2",
+      },
+    ],
+  },
+  {
+    label: "Advanced",
+    path: "/advanced",
+    children: [
+      {
+        label: "Option 1",
+        path: "/advanced/option-1",
+      },
+      {
+        label: "Option 2",
+        path: "/advanced/option-2",
+      },
+    ],
+  },
+];
